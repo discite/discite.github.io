d45758c0050711fc28a5441518c9e0ea4da69ac2
discite <4948877+discite@users.noreply.github.com>
2021-01-01 22:27:58 -0500

    Finished the basic templating

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,15 @@
-<script>
-	import successkid from 'images/successkid.jpg';
-</script>
-
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Discite's Blog</title>
+	<meta charset="UTF-8">
+
 </svelte:head>
 
-<h1>Great success!</h1>
+<h4>Discite blog</h4>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<blockquote>
+	<p><em>“Waste no more time arguing what a good man should be. Be one.” – Marcus Aurelius</em></p>
+</blockquote>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div>
+	Welcome to my little corner on this interconected world called internet, expect to find in this blog techonology blogs related to infrastructure
+</div>
